0683f1f7a09b0cd85631161eb07d4fb2c523b7d9
mark-sprott <sprotty01@gmail.com>
2018-03-05 19:52:58 +1300

    Three exercise's completed

--- eloquent.js
+++ eloquent.js
@@ -1,1 +1,42 @@
-eloquent.js
+//Minimum exercise//
+
+function min(one, two) {
+ if (one < two)
+ return one;
+ else
+ return two;
+}
+
+//**Recursion**//
+
+function isEven(number){
+  if (number == 1)
+  return false;
+  else if (number == 0)
+  return true;
+  else
+  return isEven(number -2)
+}
+
+//***Bean counting***//
+
+function countBs(str){
+  var string = str.match(/B/g).length;
+  return string;
+}
+
+console.log(countBs("BBC"));
+
+function countChar(str, char){
+  var count = 0;
+  for (var i=0; i<str.length; i++)
+    if (str.charAt(i) == char)
+    count++;
+  return count;
+}
+function countBs(str){
+  return countChar(str, "B");
+}
+
+console.log(countBs("BBBC"));
+console.log(countChar("kakkkkkkerlak", "k"));
